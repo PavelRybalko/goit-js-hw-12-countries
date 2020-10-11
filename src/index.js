c672deb11e1e98d53e9323456914457d75aa5a4f
Pavel Rybalko <freedom277f@gmail.com>
2020-10-11 13:02:36 +0300

    made last fixes, add markups func

--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 var debounce = require('lodash.debounce');
-import countriesListTemplate from './templates/countries-item-list.hbs';
-import countryItemTemplate from './templates/country-item.hbs';
+import './sass/styles.scss';
 import refs from './js/refs';
 import fetchCountries from './js/fetchCountries';
-import './sass/styles.scss';
-
-export const createListMarkup = data => {
-  const markup = countriesListTemplate(data);
-  refs.countriesContainerRef.insertAdjacentHTML('beforeend', markup);
-};
-
-export const createCountryMarkup = data => {
-  const markup = countryItemTemplate(data);
-  refs.countriesContainerRef.insertAdjacentHTML('beforeend', markup);
-};
 
 const fetchCountriesWithDebounce = debounce(() => {
   fetchCountries(refs.inputRef.value);
 }, 500);
 refs.inputRef.addEventListener('input', fetchCountriesWithDebounce);
+
+refs.countriesContainerRef.addEventListener('click', event => {
+  if (event.target.nodeName !== 'LI') return;
+  fetchCountries(event.target.textContent);
+});
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,20 +1,26 @@
 import refs from './refs';
+import countriesListTemplate from '../templates/countries-item-list.hbs';
+import countryItemTemplate from '../templates/country-item.hbs';
+import createMarkups from './createMarkups';
 import { alert, notice, info, success, error } from '@pnotify/core';
-import { createListMarkup, createCountryMarkup } from '../index';
 
-export default function fetchCountries(value) {
-  refs.countriesContainerRef.innerHTML = '';
-  const url = `https://restcountries.eu/rest/v2/name/${value}`;
+
+export default function fetchCountries(searchQuery) {
+  
+  const url = `https://restcountries.eu/rest/v2/name/${searchQuery.trim()}`;
+  if(!searchQuery) return;
 
   return fetch(url)
     .then(response => response.json())
     .then(data => {
       if (data.length > 10) {
-        error('Сделайте более специфичный запрос');
+        error('Сделайте более специфичный запрос!');
       } else if (data.length === 1) {
-        createCountryMarkup(data);
+        success('Страна найдена');
+        createMarkups(data, countryItemTemplate);
+        refs.inputRef.value = "";
       } else if (data.length > 1 && data.length <= 10) {
-        createListMarkup(data);
+        createMarkups(data, countriesListTemplate);
       }
     })
     .catch(error => console.log(error));
